f846781058e0d250e9f38005f9bf433577d16fe7
Doherty Cho <jdy0120@seerslab.com>
2024-10-09 00:18:20 +0900

    update: posts list desc from date

--- apps/webs/blog/src/pages/index.tsx
+++ apps/webs/blog/src/pages/index.tsx
@@ -1,32 +1,28 @@
-import React from "react";
-import _ from "lodash";
-import { graphql } from "gatsby";
+import React from 'react';
+import _ from 'lodash';
+import { graphql } from 'gatsby';
 
-import Layout from "../_components/c_organisms/layout";
-import SEO from "../_components/b_mocules/SEO";
-import Bio from "../_components/a_atoms/Bio";
-import PostList from "../_components/b_mocules/postlist";
-import SideTagList from "../_components/b_mocules/SideTagList";
-import Divider from "../_components/a_atoms/Divider";
-import VerticalSpace from "../_components/a_atoms/VerticalSpace";
+import Layout from '../_components/c_organisms/layout';
+import SEO from '../_components/b_mocules/SEO';
+import Bio from '../_components/a_atoms/Bio';
+import PostList from '../_components/b_mocules/postlist';
+import SideTagList from '../_components/b_mocules/SideTagList';
+import Divider from '../_components/a_atoms/Divider';
+import VerticalSpace from '../_components/a_atoms/VerticalSpace';
 
-import blogConfigs from "../configs/blog-config";
+import blogConfigs from '../configs/blog-config';
 
 const { title, description, siteUrl } = blogConfigs;
 
 const BlogIndex = ({ data }: any) => {
   const posts = data.allMarkdownRemark.nodes;
-  const tags = _.sortBy(data.allMarkdownRemark.group, [
-    "totalCount",
-  ]).reverse();
+  const tags = _.sortBy(data.allMarkdownRemark.group, ['totalCount']).reverse();
 
   if (posts.length === 0) {
     return (
       <p>
-        No blog posts found. Add markdown posts to
-        &quot;content/blog&quot; (or the directory you specified for
-        the &quot;gatsby-source-filesystem&quot; plugin in
-        gatsby-config.js).
+        No blog posts found. Add markdown posts to &quot;content/blog&quot; (or the directory you specified for the
+        &quot;gatsby-source-filesystem&quot; plugin in gatsby-config.js).
       </p>
     );
   }
@@ -52,7 +48,7 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { frontmatter: { date: ASC } }) {
+    allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
       nodes {
         excerpt(pruneLength: 200, truncate: true)
         fields {
